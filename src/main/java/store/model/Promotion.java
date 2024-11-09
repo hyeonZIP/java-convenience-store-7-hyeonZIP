d1c983e8d7eab7db809f08f25f5851e495f1ef7b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-11-10 00:51:18 +0900

    feat: 프로모션 날짜 필드 변수 추가

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -6,10 +6,14 @@ public class Promotion {
     private final String name;
     private final int buy;
     private final int get;
+    private final String startDate;
+    private final String endDate;
 
     public Promotion(List<String> promotion) {
         this.name = promotion.get(0);
         this.buy = Integer.parseInt(promotion.get(1));
         this.get = Integer.parseInt(promotion.get(2));
+        this.startDate = promotion.get(3);
+        this.endDate = promotion.get(4);
     }
 }

fe4a3e7ceb24fa4afdb6d8aca36974436217cb43
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-11-09 23:45:36 +0900

    refactor: 정규표현식 사용

--- src/main/java/store/validator/StoreValidator.java
+++ src/main/java/store/validator/StoreValidator.java
@@ -1,33 +1,32 @@
 package store.validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StoreValidator {
     private static final String FORMAT_ERROR = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+    private static final String ETC_ERROR = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
+    private static final String REGEX = "^\\[([가-힣a-zA-Z0-9]+)-([0-9]+)\\](,\\[([가-힣a-zA-Z0-9]+)-([0-9]+)\\])*$";
+    private static final String HYPHEN = "-";
 
-    public void validateBlank(String input) {
-        if (input.isBlank()) {
+    public void validateFormat(String input) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
             throw new IllegalArgumentException(FORMAT_ERROR);
         }
     }
 
-    public void validateBrakets(String input) {
-        if (!input.startsWith("[") || !input.endsWith("]")) {
-            throw new IllegalArgumentException(FORMAT_ERROR);
-        }
-    }
-
-    public void validateSize(List<String> input) {
-        if (input.size() != 2) {
-            throw new IllegalArgumentException(FORMAT_ERROR);
-        }
-    }
-
-    public void validateParsing(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(FORMAT_ERROR);
+    public void validateDuplicate(List<String> items) {
+        Set<String> dupleCheckItem = new HashSet<>();
+        for (String item : items) {
+            String name = item.split(HYPHEN)[0];
+            if (!dupleCheckItem.add(name)) {
+                throw new IllegalArgumentException(ETC_ERROR);
+            }
         }
     }
 }

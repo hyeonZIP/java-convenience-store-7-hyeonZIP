2be8f485c07aa46f0af42bdb852a3423389a472f
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-11-11 11:16:52 +0900

    test: 항목 연계 생성 테스트
    - 프로모션 항목만 들어왔을 경우 연계되어 동일 상품의 일반 항목 생성
    - 연계되어 생성된 일반 항목의 재고는 0으로 고정

--- src/test/java/store/service/StoreServiceTest.java
+++ src/test/java/store/service/StoreServiceTest.java
@@ -13,6 +13,7 @@ import store.validator.StoreValidator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,16 +44,21 @@ class StoreServiceTest {
     }
 
     @Test
-    @DisplayName("재고 파일 저장 테스트")
+    @DisplayName("항목 연계 생성 테스트")
     void saveInventory() throws IOException {
-        String inputFile = "콜라,1000,10,탄산2+1";
+        String inputFile = "콜라,1000,10,탄산2+1\n";
         BufferedReader inventory = new BufferedReader(new StringReader(inputFile));
         storeService.saveInventory(inventory);
-        for (Item item : items.getItems()) {
-            assertThat(item.getName()).isEqualTo("콜라");
-            assertThat(item.getPrice()).isEqualTo(1000);
-            assertThat(item.getQuantity()).isEqualTo(10);
-            assertThat(item.getPromotion()).isEqualTo("탄산2+1");
-        }
+        List<Item> savedItems = items.getItems();
+        assertThat(savedItems).hasSize(2);
+        assertItemEquals(savedItems.getFirst(), "콜라", 1000, 10, "탄산2+1");
+        assertItemEquals(savedItems.getLast(), "콜라", 1000, 0, "null");
+    }
+
+    private void assertItemEquals(Item item, String name, int price, int quantity, String promotion) {
+        assertThat(item.getName()).isEqualTo(name);
+        assertThat(item.getPrice()).isEqualTo(price);
+        assertThat(item.getQuantity()).isEqualTo(quantity);
+        assertThat(item.getPromotion()).isEqualTo(promotion);
     }
 }

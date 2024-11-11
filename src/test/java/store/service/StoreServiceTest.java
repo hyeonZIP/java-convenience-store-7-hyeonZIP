1c9b7aaa90c66a423c01919c82404ca7bbf559a6
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-11-11 12:16:21 +0900

    test: 항목 연계 생성 테스트
    - 프로모션 항목만 들어왔을 경우 연계되어 동일 상품의 일반 항목 생성
    - 연계되어 생성된 일반 항목의 재고는 0으로 고정

--- src/test/java/store/service/StoreServiceTest.java
+++ src/test/java/store/service/StoreServiceTest.java
@@ -12,8 +12,9 @@ import store.validator.StoreValidator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,13 +27,15 @@ class StoreServiceTest {
     private ReceiptItems receiptItems;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         items = new Items();
         promotions = new Promotions();
         receiptItems = new ReceiptItems();
         store = new Store(items, promotions, receiptItems);
         storeValidator = new StoreValidator();
         storeService = new StoreService(promotions, items, store, storeValidator);
+        BufferedReader inventory = storeService.loadInventory();
+        storeService.saveInventory(inventory);
     }
 
     @Test
@@ -45,20 +48,36 @@ class StoreServiceTest {
 
     @Test
     @DisplayName("항목 연계 생성 테스트")
-    void saveInventory() throws IOException {
-        String inputFile = "콜라,1000,10,탄산2+1\n";
-        BufferedReader inventory = new BufferedReader(new StringReader(inputFile));
-        storeService.saveInventory(inventory);
+    void saveInventory() {
         List<Item> savedItems = items.getItems();
-        assertThat(savedItems).hasSize(2);
-        assertItemEquals(savedItems.getFirst(), "콜라", 1000, 10, "탄산2+1");
-        assertItemEquals(savedItems.getLast(), "콜라", 1000, 0, "null");
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(List.of("오렌지주스", "1800", "0", "null"));
+        expected.add(List.of("탄산수", "1200", "0", "null"));
+        for (List<String> expect : expected) {
+            assertThatAnySatisfy(savedItems, expect);
+        }
+
     }
 
-    private void assertItemEquals(Item item, String name, int price, int quantity, String promotion) {
-        assertThat(item.getName()).isEqualTo(name);
-        assertThat(item.getPrice()).isEqualTo(price);
-        assertThat(item.getQuantity()).isEqualTo(quantity);
-        assertThat(item.getPromotion()).isEqualTo(promotion);
+    @Test
+    @DisplayName("")
+    void findItem() {
+        String input = "[사이다-5],[콜라-6]";
+
+        Map<List<Item>, Integer> result = storeService.findItem(input);
+
+        for (Map.Entry<List<Item>, Integer> entry : result.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+
+    private void assertThatAnySatisfy(List<Item> savedItems, List<String> expected) {
+        assertThat(savedItems).anySatisfy(savedItem -> {
+            assertThat(savedItem.getName()).isEqualTo(expected.get(0));
+            assertThat(savedItem.getPrice()).isEqualTo(Integer.parseInt(expected.get(1)));
+            assertThat(savedItem.getQuantity()).isEqualTo(Integer.parseInt(expected.get(2)));
+            assertThat(savedItem.getPromotion()).isEqualTo(expected.get(3));
+        });
     }
 }
